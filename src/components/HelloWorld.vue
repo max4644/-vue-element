183fd0bc63c2607c7486c84d053ca092134f2570
max4644 <752577868@qq.com>
2019-10-11 12:04:36 +0800

    更新

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,188 +1,341 @@
 <template>
-  <div>
-<!--    搜索查询-->
-    <el-form :inline="true" :model="filters" ref="filters">
-      <el-form-item prop="name" label="客户名称" size="small">
-        <el-input type="text" v-model="filters.name" placeholder="请输入客户名称" clearable @clear="clearListPage">
-          <i slot="prefix" class="el-input__icon el-icon-search"></i>
-          <el-button type="primary" slot="append" @click="listPage">查询</el-button>
-        </el-input>
-      </el-form-item>
-    </el-form>
-    <el-button size="mini" type="primary" @click="add">添加</el-button>
-<!--    表格-->
-    <el-table
-      :data="tableData"
-      style="width: 100%"
-    border>
-      <el-table-column
-        label="日期"
-        prop="date">
-      </el-table-column>
-      <el-table-column
-        label="姓名"
-        prop="name">
-        <template slot-scope="scope">
-          {{ scope.row.name }}
-<!--          <el-popover trigger="hover" placement="top">-->
-<!--            <p>姓名: {{ scope.row.name }}</p>-->
-<!--            <p>住址: {{ scope.row.address }}</p>-->
-<!--            <div slot="reference" class="name-wrapper">-->
-<!--              <el-tag size="medium">{{ scope.row.name }}</el-tag>-->
-<!--            </div>-->
-<!--          </el-popover>-->
-        </template>
-      </el-table-column>
-      <el-table-column label="操作">
-        <template slot-scope="scope">
-          <el-button
-            size="mini"
-            @click="change(scope.$index, scope.row)">编辑</el-button>
-          <el-button
-            size="mini"
-            type="danger"
-            @click="del(scope.$index, scope.row)">删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-<!--    弹窗-->
-    <el-dialog
-      :title="titleDialog"
-      :visible.sync="showDialog" @close="closeDialog">
-      <el-form :model="addForm" ref="addForm" :rules="addRules"  label-width="100px">
-        <el-form-item prop="name" label="客户名称" >
-          <el-input v-model="addForm.name" size="small" placeholder="请选择"></el-input>
-        </el-form-item>
-        <el-form-item prop="age" label="客户年龄" size="small">
-          <el-input-number v-model="addForm.age" :min="1" size="small" style="width: 100%;" placeholder="请选择"></el-input-number>
-        </el-form-item>
-        <el-form-item prop="sex" label="客户性别" size="small">
-          <el-select v-model="addForm.sex" placeholder="请选择" style="width: 100%;" size="small">
-            <el-option
-              v-for="item in options"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value">
-            </el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item prop="birth" label="客户出生日期" size="small">
-          <el-date-picker type="datetime" placeholder="选择日期" v-model="addForm.birth" style="width: 100%;" size="mini" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
-        </el-form-item>
-      </el-form>
-      <span slot="footer" class="dialog-footer">
-    <el-button size="mini" @click="showDialog = false">取 消</el-button>
-    <el-button size="mini" type="primary"  @click="submitForm(titleDialog)" :disabled="saveBtn">确 定</el-button>
-  </span>
-    </el-dialog>
-  </div>
+    <div>
+        <!--    搜索查询-->
+        <el-form :inline="true" :model="filters" ref="filters">
+            <el-form-item prop="name" label="客户名称" size="small">
+                <el-input type="text" v-model.trim="filters.name" placeholder="请输入客户名称" clearable
+                          @clear="clearListPage">
+                    <i slot="prefix" class="el-input__icon el-icon-search"></i>
+                    <el-button type="primary" slot="append" @click="listPage">查询</el-button>
+                </el-input>
+            </el-form-item>
+        </el-form>
+        <el-button size="mini" type="primary" @click="addTable">添加</el-button>
+
+        <!--      另一种风格搜索查询 搜索在最右侧 并且带重置-->
+        <el-form label-position="top" :model="filters" ref="filters">
+            <el-row>
+                <el-col :span="8">
+                    <el-form-item label="Full Name" size="small">
+                        <el-input type="text" v-model="filters.name" placeholder="Please Input" clearable
+                                  @clear="clearListPage">
+                        </el-input>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="3">
+                    <el-form-item label="Sex" size="small">
+                        <el-select v-model="filters.name" clearable placeholder="Please Select">
+                            <el-option value="dada" label="dada"></el-option>
+                        </el-select>
+                    </el-form-item>
+                </el-col>
+                <el-col :span="3">
+                    <el-form-item label="Date of Birth" size="small">
+                        <el-date-picker type="datetime" placeholder="Please Select" v-model="filters.name"
+                                        style="width: 19%;margin-right: 5px;" size="mini"
+                                        value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
+                    </el-form-item>
+                </el-col>
+                <el-form-item label="" size="small" class="marT35 floatR">
+                    <el-button type="primary" @click="searchData">SEARCH</el-button>
+                    <el-button @click="resetData">RESET</el-button>
+                </el-form-item>
+            </el-row>
+        </el-form>
+
+        <!--    表格-->
+        <el-table
+                :data="tableData"
+                style="width: 100%"
+                border>
+            <el-table-column
+                    label="日期"
+                    prop="date">
+            </el-table-column>
+            <el-table-column
+                    label="姓名"
+                    prop="name">
+            </el-table-column>
+            <el-table-column
+                    label="Status"
+                    prop="date">
+                <template slot-scope="scope">
+                    <el-switch
+                            v-model="scope.row.status"
+                            active-color="#308CE3"
+                            inactive-color="#ccc"
+                            :active-value="'1'"
+                            :inactive-value="'0'"
+                            @change="changeSwitch($event,scope.row)">
+                    </el-switch>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作">
+                <template slot-scope="scope">
+                    <el-button type="text"
+                               size="mini"
+                               @click="editTable(scope.$index, scope.row)">Edit
+                    </el-button>
+                    <el-button type="text"
+                               size="mini"
+                               @click="viewTable(scope.$index, scope.row)">View
+                    </el-button>
+                    <el-button type="text"
+                               size="mini"
+                               @click="deleteTable(scope.$index, scope.row)">Delete
+                    </el-button>
+                </template>
+            </el-table-column>
+        </el-table>
+        <!--    弹窗-->
+        <el-dialog
+                :title="titleDialog"
+                :visible.sync="showDialog" @close="closeDialog">
+            <el-form :model="form" ref="forms" :rules="rules" label-width="100px">
+                <el-form-item prop="name" label="客户名称">
+                    <el-input v-model="form.name" size="small" placeholder="请选择" :disabled="showInputs"></el-input>
+                </el-form-item>
+                <el-form-item prop="age" label="客户年龄" size="small">
+                    <el-input-number v-model="form.age" :min="1" size="small" style="width: 100%;"
+                                     placeholder="请选择" :disabled="showInputs"></el-input-number>
+                </el-form-item>
+                <el-form-item prop="sex" label="客户性别" size="small">
+                    <el-select v-model="form.sex" placeholder="请选择" style="width: 100%;" size="small" :disabled="showInputs">
+                        <el-option
+                                v-for="item in options"
+                                :key="item.value"
+                                :label="item.label"
+                                :value="item.value">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item prop="birth" label="客户出生日期" size="small">
+                    <el-date-picker type="datetime" placeholder="选择日期" v-model="form.birth" style="width: 100%;"
+                                    size="mini" value-format="yyyy-MM-dd HH:mm:ss"></el-date-picker>
+                </el-form-item>
+            </el-form>
+            <div slot="footer" class="dialog-footer">
+                <el-button type="primary" size="small" @click="saveDialog(titleDialog)" v-show="!showInputs" :disabled="showSaveBtn">SAVE</el-button>
+                <el-button  @click="showDialog = false">CLOSE</el-button>
+            </div>
+        </el-dialog>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      filters: {
-        name: '',
-      },
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }],
-      showDialog: false,
-      addForm: {
-        name: '',
-        age: 1,
-        sex: '',
-        birth: ''
-      },
-      addRules: {
-        name: [ { required: true, message: '请输入', trigger: 'blur' }],
-        age: [ { required: true, message: '请输入', trigger: 'blur' }],
-        sex: [ { required: true, message: '请选择', trigger: 'change' }],
-        birth: [ { required: true, message: '请选择', trigger: 'change' }],
-      },
-      options: [{
-        value: '0',
-        label: '男'
-      }, {
-        value: '1',
-        label: '女'
-      }],
-      saveBtn: false,
-      titleDialog: '添加'
-    }
-  },
-  methods: {
-    listPage () {
-      console.log('查询页面' + this.filters)
-    },
-    clearListPage () {
-      this.filters = {
-        name: '',
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        filters: {
+          page: 1,
+          pageSize: 10
+        },
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        }],
+        showDialog: false,
+        form: {
+          name: '',
+          age: 1,
+          sex: '',
+          birth: ''
+        },
+        rules: {
+          name: [{required: true, message: '请输入', trigger: 'blur'}],
+          age: [{required: true, message: '请输入', trigger: 'blur'}],
+          sex: [{required: true, message: '请选择', trigger: 'change'}],
+          birth: [{required: true, message: '请选择', trigger: 'change'}],
+        },
+        options: [{
+          value: '0',
+          label: '男'
+        }, {
+          value: '1',
+          label: '女'
+        }],
+        saveBtn: false,
+        titleDialog: '添加',
+        showSaveBtn: false,
+        showInputs: false,
       }
-      this.listPage()
     },
-    del(index, row) {
-      this.$confirm('此操作将删除该数据, 是否继续?', '提示', {
-        confirmButtonText: '确定',
-        cancelButtonText: '取消',
-        type: 'warning'
-      }).then(() => {
-        this.$message({
-          type: 'success',
-          message: '删除成功'
-        });
-      }).catch(() => {
-        this.$message({
-          type: 'info',
-          message: '已取消删除'
-        });
-      })
-    },
-    // 提交弹窗的表格
-    submitForm(name) {
-      this.saveBtn = true
-      if (name === '添加') {
-        console.log('添加')
-        this.insert(this.addForm)
-      } else {
-        console.log('编辑')
-        this.update(this.addForm)
-      }
-      setTimeout(() => {
-        this.saveBtn = false
-      }, 1500)
-    },
-    insert(addForm) {
-      // 正式添加
-      this.$refs.addForm.validate((valid) => {
-        if (valid) {
-          console.log('submit!');
-        } else {
-          console.log('error submit!!');
-          return false;
-        }
-      });
-    },
-    update(addForm) {
-      // 正式编辑
-    },
-    add() {
-      this.showDialog = true
-      this.titleDialog = '添加'
-    },
-    change() {
-      this.showDialog = true
-      this.titleDialog = '编辑'
-    },
-    closeDialog() {
+    methods: {
+      listPage() {
+        console.log('查询页面' + this.filters)
+      },
+      // 每页条数
+      handleSizeChange(val) {
+        this.filters.pageSize = val
+        this.listPage()
+      },
+      // 当前页
+      handleCurrentChange(val) {
+        this.filters.page = val
+        this.listPage()
+      },
+      // 重置搜索条件
+      resetData() {
+        this.filters = this.$options.data().filters
+        this.listPage()
+      },
+      clearListPage() {
+      },
+      // 查询数据
+      searchData() {
+        this.listPage()
+      },
+      // 查询单条
+      getInfoById(ids) {
+        this.$http.get('', {
+          params: {
+            id: ids
+          }
+        }).then((res) => {
+          if (res.data.status === 200) {
+            this.form = res.data.data
+            this.showDialog = true
+          } else {
+            this.erFn(res.data.msg)
+          }
+        }).catch((error) => {
+          this.erFn(error);
+        })
+      },
+      // 增加表格数据
+      addTable() {
+        this.titleDialog = 'Add'
+        this.showDialog = true
+      },
+      // 修改表格数据
+      editTable(index, row) {
+        this.titleDialog = 'Edit'
+        this.getInfoById(row.id)
+      },
+      // 查看表格数据
+      viewTable(index, row) {
+        this.titleDialog = 'View'
+        this.getInfoById(row.id)
+      },
+      // 删除表格数据
+      deleteTable(index, row) {
+        this.$confirm('Do you confirm deletion?', 'Delete', {
+          confirmButtonText: 'Yes',
+          cancelButtonText: 'No',
+          type: 'warning'
+        }).then(() => {
+          this.$http.get('', {
+            params: {
 
+            }
+          }).then((res) => {
+            if (res.data.status === 200) {
+              this.suFn(res.data.msg)
+              this.listPage()
+            } else {
+              this.erFn(res.data.msg)
+            }
+          }).catch((error) => {
+            this.erFn(error);
+          })
+        }).catch(() => {
+          this.$message({
+            type: 'info',
+            message: 'Canceled deletion'
+          });
+        });
+      },
+      // 新增数据
+      addData() {
+        this.$http.post('', this.form).then((res) => {
+          if (res.data.status === 200) {
+            this.suFn(res.data.msg)
+            this.showDialog = false
+            this.listPage()
+          } else {
+            this.erFn(res.data.msg)
+          }
+        }).catch((error) => {
+          this.erFn(error);
+        })
+      },
+      // 修改数据
+      updateData() {
+        this.$http.post('', this.form).then((res) => {
+          if (res.data.status === 200) {
+            this.suFn(res.data.msg)
+            this.showDialog = false
+            this.listPage()
+          } else {
+            this.erFn(res.data.msg)
+          }
+        }).catch((error) => {
+          this.erFn(error);
+        })
+      },
+      // 提交弹窗的表格
+      saveDialog(name) {
+        /* eslint-disable */
+        this.showSaveBtn = true
+        this.$refs.forms.validate((valid) => {
+          if (valid) {
+            switch (title) {
+              case 'Add':
+                this.addData()
+                break;
+              case 'Edit':
+                this.updateData()
+                break;
+            }
+          } else {
+            this.waFn('Please fill in the required items.')
+            return false;
+          }
+        });
+        setTimeout(() => {
+          this.showSaveBtn = false
+        }, 1500)
+        /* eslint-disable */
+      },
+      insert(addForm) {
+        // 正式添加
+        this.$refs.addForm.validate((valid) => {
+          if (valid) {
+            console.log('submit!');
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      },
+      // 关闭弹窗
+      closeDialog() {
+        this.form = this.$options.data().form
+        this.$refs.forms.resetFields();
+      },
+      // 状态改变
+      changeSwitch(event,row) {
+        this.$http.get('/rule/focusPaxRule/updateByStatus', {
+          params: {
+            id: row.id,
+            status: event,
+            version: row.version
+          }
+        }).then((res) => {
+          if (res.data.status === 200) {
+            this.suFn(res.data.msg)
+            this.listPage()
+          } else {
+            this.erFn(res.data.msg)
+          }
+        }).catch((error) => {
+          this.erFn(error);
+        })
+      },
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
---
+++ src/components/tableTest.vue
@@ -0,0 +1,305 @@
+<template>
+    <div >
+        <div class="flex-container" >
+            <div class="flex-item item4" v-for="item in viewList">
+                <div style="display: flex;" >
+                    <div class="flex-item item1" >
+                        <div v-for="(title,index) in item.titleList">{{title}}</div>
+                    </div>
+                    <div class="flex-item item1" >
+                        <div v-for="(content,index) in item.contentList">{{content}}</div>
+                    </div>
+                </div>
+        </div>
+        </div>
+<!--        <div class="box2">-->
+<!--            box2:多行多列-->
+<!--            <ul>-->
+<!--                <li v-for="i in data">-->
+<!--                    <div class="com" >-->
+<!--                        <div >{{i}}</div>-->
+<!--                    </div>-->
+<!--                </li>-->
+<!--            </ul>-->
+<!--        </div>-->
+<!--            </div>-->
+<!--        </div>-->
+
+<!--        <ul class="flexcontainer">-->
+<!--            <li>Something</li>-->
+<!--            <li>Fox Example</li>-->
+<!--            <li>All About Web</li>-->
+<!--            <li>Fox Example</li>-->
+<!--            <li>Something</li>-->
+<!--            <li>Fox Example</li>-->
+<!--            <li>Something</li>-->
+<!--            <li>Fox Example</li>-->
+<!--        </ul>-->
+
+<!--        <div class="box1">-->
+<!--            box1:实现两列多行布局-->
+<!--            <ul>-->
+<!--                <li>111</li>-->
+<!--                <li>222</li>-->
+<!--                <li>333</li>-->
+<!--                <li>111</li>-->
+<!--                <li>222</li>-->
+<!--                <li>333</li>-->
+<!--                <li>111</li>-->
+<!--                <li>222</li>-->
+<!--                <li>333</li>-->
+<!--                <li>111</li>-->
+<!--                <li>222</li>-->
+<!--                <li>333</li>-->
+<!--                <li>111</li>-->
+<!--                <li>222</li>-->
+<!--                <li>333</li>-->
+
+<!--            </ul>-->
+<!--        </div>-->
+
+
+
+
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'tableTest',
+    data() {
+      return{
+        data: [
+          '刘涛为胡歌庆生',
+          '高雷雷炮轰足协',
+          '刘涛为胡歌庆生',
+          '高雷雷炮轰足协',
+        ],
+        list: [
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+          '高雷雷炮轰足协',
+        ],
+        viewList: [
+          {
+            titleList: [
+              'Carrier',
+              'Departure Airport code',
+              'Arrival City code',
+              'Arrival day',
+              'Intermeditate Airports',
+              'General Aircraft type code',
+              'Freight',
+              'Passenger Classs 4',
+              'Passenger Classs 8',
+              'Restriction',
+              'Full Routing',
+              'Multi Carrier Designator',
+              'Ghost Flight',
+              'Duplicate Carrier 2',
+              'Duplicate Carrier 4',
+              'Duplicate Carrier 6',
+              'Duplicate Carrier 8',
+              'Duplicate Carrier 10',
+              'Duplicate Carrier 12',
+              'Duplicate Carrier 14'
+            ],
+            contentList: [
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎'
+            ]
+          },
+          {
+            titleList: [
+              'Flight No.',
+              'Departure City code',
+              'Arrival Country code',
+              'Elapsed journey time',
+              'Aircraft Change',
+              'Specific Aircraft type code',
+              'Passenger Class 1',
+              'Passenger Class 5',
+              'Passenger Class 9',
+              'Domestic/International marker',
+              'Longest Sector',
+              'Flight Duplication',
+              'Duplicate',
+              'Duplicate Flight 2',
+              'Duplicate Flight 4',
+              'Duplicate Flight 6',
+              'Duplicate Flight 8',
+              'Duplicate Flight 10',
+              'Duplicate Flight 12',
+              'Duplicate Flight 14'
+            ],
+            contentList: [
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎'
+            ]
+          },
+          {
+            titleList: [
+              'Carrier 2',
+              'Departure Airport code',
+              'Departure time',
+              'Days of Operation',
+              'Government Approval',
+              'Service type',
+              'Passenger Class 2',
+              'Passenger Class 6',
+              'Passenger Class 10',
+              'Effective from date',
+              'Distance',
+              'Aircraft Owner',
+              'Duplicate Carrier 1',
+              'Duplicate Carrier 3',
+              'Duplicate Carrier 5',
+              'Duplicate Carrier 7',
+              'Duplicate Carrier 9',
+              'Duplicate Carrier 11',
+              'Duplicate Carrier 13',
+              'Duplicate Carrier 15'
+            ],
+            contentList: [
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎'
+            ]
+          },
+          {
+            titleList: [
+              'Flight No.2',
+              'Arrival Airport code',
+              'Arrival time',
+              'Stops',
+              'Comment 010/050',
+              'Available seats',
+              'Passenger Class 3',
+              'Passenger Class 7',
+              'Freight Classes',
+              'Effective to date',
+              'Shared Airline Designator',
+              'Operating/Non Operating marker',
+              'Duplicate Flight 1',
+              'Duplicate Flight 3',
+              'Duplicate Flight 5',
+              'Duplicate Flight 7',
+              'Duplicate Flight 9',
+              'Duplicate Flight 11',
+              'Duplicate Flight 13',
+              'Duplicate Flight 15'
+            ],
+            contentList: [
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎',
+              '王小虎'
+            ]
+          }
+        ]
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style>
+    .flex-container {
+        display: -webkit-flex;
+        display: flex;
+        width: 100%;
+        background-color: lightgrey;
+    }
+
+    .flex-item {
+        background-color: cornflowerblue;
+        margin: 10px;
+    }
+    .flex-item1 {
+        background-color: palegreen;
+        margin: 10px;
+        display: -webkit-flex;
+        display: flex;
+    }
+
+    .item1 {
+        flex: 1;
+    }
+
+    .item4 {
+        flex: 1;
+    }
+    .flexcontainer {
+        width: 600px;
+        display: flex;
+        flex-flow: row wrap;
+    }
+    .flexcontainer li {
+        width: 25%;
+        background-color: pink;
+    }
+    .box1 {
+        width: 500px;
+        background: #EEEEEE;
+    }
+    .box1 ul {
+        clear: both;
+        overflow: hidden;
+    }
+    .box1 ul li {
+        width: 48%;
+        height: 100px;
+        margin-bottom: 10px;
+        background: skyblue;
+        float: left;
+    }
+    .box1 ul li:nth-child(even) {
+        margin-left: 4%;
+    }
+    .box2 {
+        background: #EEEEEE;
+        margin-top: 20px;
+        width: 500px;
+    }
+    .box2 ul {
+        overflow: hidden;
+        margin-left: -10px;
+        background: #EEEEEE;
+    }
+    .box2 ul li {
+        width: 25%;
+        height: 50px;
+        float: left;
+        padding-left: 10px;
+        box-sizing: border-box;
+        margin-bottom: 10px;
+    }
+    .box2 ul li .com {
+        height: inherit;
+        background: skyblue;
+    }
+</style>
